992287732e9dc9edfc02f87dfd91d846b29ba8d7
Matteo Muraca <mmuraca247@gmail.com>
2024-05-25 19:32:08 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ETH Signer
+
+This tool is a wrapper for [ethers-signer](https://docs.rs/ethers/latest/ethers/signers/index.html) whose only purpose at the moment is to showcase the Address for a private key, passed as an argument or via stdin.
+
+I might add more functionality in the future.
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use ethers_signers::{LocalWallet, Signer};
+use std::str::FromStr;
+
+fn main() {
+    let mut private_key = std::env::args().nth(1).unwrap_or_default();
+    if private_key.len() == 0 {
+        std::io::stdin()
+            .read_line(&mut private_key)
+            .expect("Failed to read private key!");
+
+        private_key = private_key.trim().to_string();
+    }
+
+    private_key = private_key.replace("\"", "");
+
+    let wallet = LocalWallet::from_str(private_key.as_str()).expect("Invalid private key!");
+
+    println!("Address: {:?}", wallet.address());
+}
